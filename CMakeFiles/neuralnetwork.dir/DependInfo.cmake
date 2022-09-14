c6a2d32c80574c12b53acc173bdfab4a227f800a
gramcracker <watermelonz656@gmail.com>
2022-09-13 23:38:32 -0500

    updated submodule

---
+++ CMakeFiles/neuralnetwork.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/josh/workspace/git/neural_network/src/data_operations.cpp" "/home/josh/workspace/git/neural_network/CMakeFiles/neuralnetwork.dir/src/data_operations.cpp.o"
+  "/home/josh/workspace/git/neural_network/src/link.cpp" "/home/josh/workspace/git/neural_network/CMakeFiles/neuralnetwork.dir/src/link.cpp.o"
+  "/home/josh/workspace/git/neural_network/src/net.cpp" "/home/josh/workspace/git/neural_network/CMakeFiles/neuralnetwork.dir/src/net.cpp.o"
+  "/home/josh/workspace/git/neural_network/src/neuron.cpp" "/home/josh/workspace/git/neural_network/CMakeFiles/neuralnetwork.dir/src/neuron.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "neuralnetwork_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/neuralnetwork.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neuralnetwork.dir/src/data_operations.cpp.o"
+  "CMakeFiles/neuralnetwork.dir/src/link.cpp.o"
+  "CMakeFiles/neuralnetwork.dir/src/net.cpp.o"
+  "CMakeFiles/neuralnetwork.dir/src/neuron.cpp.o"
+  "libneuralnetwork.pdb"
+  "libneuralnetwork.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neuralnetwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
